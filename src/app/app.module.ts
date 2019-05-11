b2eb7222ef36a4519596f9c0edfd3a2a79321156
Alexander K <alexkzm@gmail.com>
2019-05-11 19:53:03 -0400

    tricks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,4 +35,15 @@ function printThis<T> (content: T): T {
 }
 
 const getProducts = function<T>(id?: T):
-  T extends number ? Product : Product []
+  T extends number ? Product : Product [];
+
+type numFunc<T> = (arg: T) => (x: number) => number;
+
+type Readonly<T> = {
+  readonly [P in keyof T]: T[P];
+}
+
+function filterBy<T, P extends keyof T> (
+              property: P, value: T[P], array: T[]){
+                return array.filter(item => item[property] === item);
+              }
